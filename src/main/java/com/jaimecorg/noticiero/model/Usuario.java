3443ff4eb227d05a9bee7befbccf7817e43f9930
Jaime Corpas <jaime.corpas.785@estudiante.iesoretania.es>
2022-11-17 10:57:32 +0100

    first commit

---
+++ src/main/java/com/jaimecorg/noticiero/model/Usuario.java
@@ -0,0 +1,65 @@
+package com.jaimecorg.noticiero.model;
+
+public class Usuario {
+    private String codigo;
+    private String nombre;
+    private Boolean admin;
+  
+    public Usuario() {
+    }
+    
+    public Usuario(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        return true;
+    }
+
+    
+}
---
+++ src/test/java/com/jaimecorg/noticiero/NoticieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaimecorg.noticiero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
